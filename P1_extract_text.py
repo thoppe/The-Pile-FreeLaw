75c79c2bb5912085c402945b8564d128c1281322
thoppe <travis.hoppe@gmail.com>
2021-12-15 12:42:26 -0500

    Add more record types to find text, make processing happen in parallel

--- P1_extract_text.py
+++ P1_extract_text.py
@@ -6,13 +6,14 @@ import json
 import tempfile
 import bs4
 import jsonlines
+from dspipe import Pipe
 from wasabi import msg
 
 f_tar = "data/source/opinions_all.tar"
 f_save = "data/FreeLaw_Opinions.jsonl"
 
 
-def data_iter():
+def jurisdiction_iter():
 
     # Handle nested tars
     with tarfile.open(f_tar, "r") as ALL:
@@ -23,11 +24,18 @@ def data_iter():
                 ALL.extract(packet, path=tmp_dir)
                 f_subtar = Path(tmp_dir) / packet.name
 
+                with open(f_subtar, "rb") as FIN:
+                    raw = FIN.read()
+
+                yield packet.name, io.BytesIO(raw)
+
+            """
                 with tarfile.open(f_subtar, "r") as TAR:
                     for k, subpacket in enumerate(TAR):
 
                         js = TAR.extractfile(subpacket).read()
                         yield packet.name, subpacket.name, js
+            """
 
 
 def idempotent(x):
@@ -43,6 +51,10 @@ field_order = [
     ("plain_text", idempotent),
     ("html", html2text),
     ("html_lawbox", html2text),
+    ("html_columbia", html2text),
+    ("html_anon_2020", html2text),
+    ("html_with_citations", html2text),
+    ("xml_harvard", html2text),
 ]
 
 error_str = (
@@ -65,7 +77,7 @@ def parse_json(item):
             text = func(js[k])
 
     if text is None:
-        msg.fail(f"Could not parse {name0} {name1}")
+        msg.fail(f"No text found for {name0} {name1}")
         return None
 
     meta = {}
@@ -78,11 +90,23 @@ def parse_json(item):
     return data
 
 
+def data_iter(name0, tar_bytes):
+
+    with tarfile.open(fileobj=tar_bytes) as TAR:
+        for k, subpacket in enumerate(TAR):
+            raw_json = TAR.extractfile(subpacket).read()
+            name1 = subpacket.name
+            yield name0, name1, raw_json
+
+
 with jsonlines.open(f_save, "w") as FOUT:
-    for item in tqdm(data_iter()):
-        js = parse_json(item)
 
-        if js is None:
-            continue
+    for name0, tar_bytes in jurisdiction_iter():
+        msg.info(f"Processing {name0}")
 
-        FOUT.write(js)
+        for js in Pipe(data_iter(name0, tar_bytes))(parse_json, -1):
+
+            if js is None:
+                continue
+
+            FOUT.write(js)
